26163bc7357444b1929bf2205eb95a96da3b6d15
drorya2324 <drorminush@gmail.com>
2020-04-18 21:20:38 +0300

    adding Json files

--- Node2D.gd
+++ Node2D.gd
@@ -2,44 +2,50 @@ extends Node2D
 
 var player_words=[]
 var current_story
+var strings
 var prompt_counter=0
 
 func _ready():
 	set_random_story()
-	#$BlackBoard/ProgressCounter.max_value=current_story.plugin_words.size()+1
-	#print(prompt_counter)
 	$BlackBoard/TextBox.clear()
 	$BlackBoard/AgainButton.hide()
 	$BlackBoard/AgainButton/AgainLabel.hide()
-	var welcome="Welcome to my Loony Lips game!\nHere we are going to create funny stories, and have a great time!\nReady?\n\nCan I have " + current_story.plugin_words[player_words.size()] + ", please?"
+	strings= get_fron_jason("Strings.json")
+	var welcome= strings["welcome"]
 	$BlackBoard/StoryText.text=(welcome)
+	prompt_player()
+	#var welcome="Welcome to my Loony Lips game!\nHere we are going to create funny stories, and have a great time!\nReady?\n\nCan I have " + current_story.plugin_words[player_words.size()] + ", please?"
+	
 	
 func set_random_story():
 	var stories=get_fron_jason("Stories.json")
-#	print("stories=",stories)
 	randomize()
 	current_story=stories[ randi() % stories.size()]
+	
 	
 func get_fron_jason(filename):
 	var file=File.new()# the file object
 	file.open(filename,File.READ)#assumes the file exists
 	var text= str(file.get_as_text())
-	print("text type is: ",text)
 	var data=parse_json(text)
-	print ("data= ",data)
 	file.close()
 	return data
 
+
 func _on_OkButton_pressed():
 	var new_text=$BlackBoard/TextBox.get_text()
-	_on_TextBox_text_entered(new_text)
+	handle_text(new_text)
 
 
 func _on_TextBox_text_entered(new_text):
-	player_words.append(new_text)
-	$BlackBoard/TextBox.clear()
-	check_player_words_length()
+	handle_text(new_text)
 
+
+func handle_text(myText):
+	player_words.append(myText)
+	$BlackBoard/TextBox.clear()
+	$BlackBoard/StoryText.clear()
+	check_player_words_length()
 
 
 func check_player_words_length():
@@ -62,18 +68,18 @@ func show_story():
 
 
 func prompt_player():
-	$BlackBoard/StoryText.text= "Can I have " + current_story.plugin_words[player_words.size()] + ", please?"
-#	print("plugin words size=",current_story.plugin_words.size())
-	_on_ProgressCounter_ready(current_story.plugin_words.size())
+	var next_plugin_word = current_story["plugin_words"][player_words.size()]
+#	$BlackBoard/StoryText.text= "Can I have " + current_story.plugin_words[player_words.size()] + ", please?"
+	$BlackBoard/StoryText.text +=(strings["prompt"] % next_plugin_word)
+	change_progress_bar()
 	
+	
+func change_progress_bar():
+	$BlackBoard/ProgressCounter.value=prompt_counter
+	var precent_counter= 100/ (current_story.plugin_words.size())
+	prompt_counter= prompt_counter+precent_counter
+
 
 #reset the game:
 func _on_AgainButton_pressed():
 	get_tree().reload_current_scene()
-
-
-
-func _on_ProgressCounter_ready(plugin_words_length):
-	var precent_counter= 100/ (plugin_words_length)
-	prompt_counter= prompt_counter+precent_counter
-	$BlackBoard/ProgressCounter.value=prompt_counter
